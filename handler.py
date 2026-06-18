e989d3bec50aae198f3110e13090be1c6600584d
MoveDetails <patrick@movedetails.com>
2026-06-18 00:55:04 -0400

    Change video filter from crop to scale

--- handler.py
+++ handler.py
@@ -114,7 +114,7 @@ def handler(job: dict) -> dict:
         transcode_args = [
             "-y", "-i", raw_path,
             "-map", "0:v:0",
-            "-vf", "crop=trunc(iw/2)*2:trunc(ih/2)*2",
+            "-vf", "scale=trunc(iw/2)*2:trunc(ih/2)*2",
             "-c:v", "libx264", "-preset", "fast", "-crf", "23",
         ]
         if has_audio:
@@ -133,7 +133,6 @@ def handler(job: dict) -> dict:
         _run_ffmpeg(
             [
                 "-y", "-i", raw_path,
-                "-vf", "crop=trunc(iw/2)*2:trunc(ih/2)*2",
                 "-frames:v", "1",
                 "-update", "1",
                 thumbnail_local,
